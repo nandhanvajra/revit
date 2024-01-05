d99337e90e4f7c533d2622cc4f402a45e6b966a2
nandhan <nandhanvajra5@gmail.com>
2024-01-05 20:49:28 +0530

    first commit nothing muxh here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from 'react'
+import {RiSearchLine} from 'react-icons/ri'
+export default function Main(){
+  return(
+    <>
+    <div className='container' style={{
+      backgroundColor:'black',
+      paddingBottom:'60rem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    }}>
+      <div style={{
+        display:'flex',
+        flexDirection:'column',
+       
+        alignItems:'center'
+
+      }}>
+    <div style={{color:'white',fontSize:'5rem'}}>REV.IT</div>
+    <div>
+      <Search style={{padding:'60px'}}></Search>
     </div>
-  );
+    </div>
+    <div style={{fontSize:'5rem',color:'white',fontWeight:'bold',textAlign:'center'}}>
+      Search to Rate
+     
+    </div>
+    </div>
+    </>
+  )
+}
+function Search({style}){
+  return(
+    <>
+      <div style={style}> 
+        <input style={{padding:'10px',margin:'6px',borderRadius:'30px',border:'none',outline:'none'}} type="text" id="searchm" placeholder='search a movie or tvshow'></input>
+        <RiSearchLine style={{color:'white',cursor:'pointer',marginLeft:'15px'}}className="search-icon" />
+      </div>
+    </>
+  )
 }
 
-export default App;
